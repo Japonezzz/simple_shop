9d42419d02baa68809a202402d2bc674bcbfef7e
Den4ick41 <511465d>
2018-03-25 16:23:19 +0300

    First form and controller added (2.0)

--- src/main/java/view/FXML_Shop_Controller.java
+++ src/main/java/view/FXML_Shop_Controller.java
@@ -1,18 +1,21 @@
 package view;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXListView;
-import com.jfoenix.controls.JFXRadioButton;
-import com.jfoenix.controls.JFXTextField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FXML_Shop_Controller implements Initializable {
+public class FXML_Shop_Controller implements Initializable{
+
+    @FXML
+    private JFXTreeTableView<?> ttv_info;
 
     @FXML
     private JFXButton btn_goods;
@@ -21,16 +24,16 @@ public class FXML_Shop_Controller implements Initializable {
     private JFXTextField fld_search;
 
     @FXML
+    private JFXButton b_search;
+
+    @FXML
     private JFXRadioButton rb_down;
 
     @FXML
-    private JFXListView<?> lv_info;
-
-    @FXML
-    private JFXComboBox<?> cb_search;
-
-    @FXML
     private JFXRadioButton rb_up;
+
+    @FXML
+    private JFXListView<?> lv_category;
 
     @FXML
     void OnActionBtn_goods(ActionEvent event) {
@@ -39,6 +42,11 @@ public class FXML_Shop_Controller implements Initializable {
 
     @FXML
     void OnActionFld_search(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionB_search(ActionEvent event) {
 
     }
 
@@ -52,10 +60,6 @@ public class FXML_Shop_Controller implements Initializable {
 
     }
 
-    @FXML
-    void OnActionCB_search(ActionEvent event) {
-
-    }
     public void initialize(URL location, ResourceBundle resources) {
 
     }
