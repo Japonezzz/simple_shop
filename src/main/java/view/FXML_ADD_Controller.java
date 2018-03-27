66013565758a65631209d019474652608267ec22
Japonezzz <Japonezzz@bigmir.net>
2018-03-27 17:05:15 +0300

    bug_fix

--- src/main/java/view/FXML_ADD_Controller.java
+++ src/main/java/view/FXML_ADD_Controller.java
@@ -1,5 +1,7 @@
 package view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,13 +10,34 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FXML_ADD_Controller implements Initializable {
+
+    @FXML
+    private AnchorPane main_pain;
+
+    @FXML
+    private JFXTextField field_ram2;
+
+    @FXML
+    private JFXTextField field_cpu2;
+
+    @FXML
+    private JFXTextField field_ramgp2;
+
+    @FXML
+    private JFXTextField field_cpugp2;
+
+    @FXML
+    private ComboBox<String> combo_categories;
 
     @FXML
     private Label label_name;
@@ -134,8 +157,79 @@ public class FXML_ADD_Controller implements Initializable {
 
     }
 
+    @FXML
+    void OnAction_categories(ActionEvent event) {
+        
+        Integer selected = combo_categories.getSelectionModel().getSelectedIndex();
+        for (Node a:main_pain.getChildren()) {
+            if(!(a.getId()!="combo_categories") || !(a.getId()!="main_label") || !(a.getId()!="Add_button") || !(a.getId()!="Cancel_button"))
+                a.setVisible(false);
+        }
+        switch (selected)
+        {
+            case 0:
+            {
+                label_name.setVisible(true);
+                field_name.setVisible(true);
+                label_quntity.setVisible(true);
+                field_quantity.setVisible(true);
+                label_desc.setVisible(true);
+                field_desc.setVisible(true);
+                label_price.setVisible(true);
+                field_price.setVisible(true);
+                label_category.setVisible(true);
+                field_category.setVisible(true);
+                label_guarantee.setVisible(true);
+                field_gurantee.setVisible(true);
+                label_RAM.setVisible(true);
+                field_ram.setVisible(true);
+                field_ram2.setVisible(true);
+                label_CPU.setVisible(true);
+                field_cpu.setVisible(true);
+                field_cpu2.setVisible(true);
+                label_cpu_gp.setVisible(true);
+                field_cpugp.setVisible(true);
+                field_cpugp2.setVisible(true);
+                label_ram_gp.setVisible(true);
+                field_ramgp.setVisible(true);
+                field_ramgp2.setVisible(true);
+                break;
+            }
+            case 1:
+            {
+                label_name.setVisible(true);
+                field_name.setVisible(true);
+                label_quntity.setVisible(true);
+                field_quantity.setVisible(true);
+                label_desc.setVisible(true);
+                field_desc.setVisible(true);
+                label_price.setVisible(true);
+                field_price.setVisible(true);
+                label_category.setVisible(true);
+                field_category.setVisible(true);
+                label_guarantee.setVisible(true);
+                field_gurantee.setVisible(true);
+                label_RAM.setVisible(true);
+                field_ram.setVisible(true);
+                field_ram2.setVisible(true);
+                label_CPU.setVisible(true);
+                field_cpu.setVisible(true);
+                field_cpu2.setVisible(true);
+                label_OS.setVisible(true);
+                field_os.setVisible(true);
+                label_diagonal.setVisible(true);
+                field_diagonal.setVisible(true);
+                label_timework.setVisible(true);
+                field_timework.setVisible(true);
+            }
+        }
+    }
+
 
     public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "PC", "Смартфони", "Одяг", "Овочі", "Вода", "Солодка вода", "Солодощі");
+        combo_categories.setItems(items);
 
     }
 }
--- src/main/java/view/FXML_Shop_Controller.java
+++ src/main/java/view/FXML_Shop_Controller.java
@@ -231,31 +231,10 @@ public class FXML_Shop_Controller implements Initializable{
         lv_category.setItems(items);
     }
     private void InitGoods(){
-        //StoreController store = new StoreController();
-        goods = new ArrayList();
-        goods.add(new Water(3, "Dobra Voda", 10, "Voda z krana", (double)1, "Water"));
-        goods.add( new Cellphone(1, "iPhone", 20, "Sell Phone from Apple", (double) 400, "CellPhone",
-                12, new RAM(12, "DDR5"), new CPU(5, 1.2),
-                "IOS", 5.2, 10));
-        goods.add(new Computer(2, "DELL", 15, "Famous NoteBook in the world", (double)800, "Computer", 12,
-                new RAM(12, "DDR5"), new CPU(5, 7.2),
-                new CPU(2, 5.0), new RAM(4, "DDR4")));
-        goods.add(new SweetDrinks(4, "Dobra Voda z limonom", 10, "Voda z krana", (double)1, "SweetDrinks",
-                new DateTerm(new Date(2018,01,20), (double)27,
-                        12)));
-        goods.add( new Sweets(5, "Romashka", 10, "Mnogo kroxmalia", (double)1, "Sweets",
-                new DateTerm(new Date(2018,03,20), (double)22,
-                        2)));
-        goods.add(new Vegetables(6, "Kartoha", 10, "Vkusno", (double)1, "Vegetables",
-                new DateTerm(new Date(2018,03,20), (double)15,
-                        4), "best of the best"));
-        goods.add(new Clothes(7, "Krosovki", 10, "Kloviu brend", (double)1,"Clothes",
-                new Date(2018, 02, 20), "Adibas",
-                2000, 20));
-
-        StoreController control = new StoreController(goods);
+        DataInfo dataInfo = new DataInfo();
+        dataInfo.InitTestData();
+        goods = dataInfo.getGoods();
         FillingListView(goods);
-
     }
     private void FillingListView(ArrayList<Goods> Items){
         ObservableList<Goods> goodsCollection = FXCollections.observableArrayList(Items);
