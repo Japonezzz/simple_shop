c93496b9f055be336af57f33f2992b2d61b99590
Japonezzz <Japonezzz@bigmir.net>
2018-03-30 18:43:16 +0300

    In goods form tableview is functioning, search is working

--- src/main/java/view/FXML_Goods_Controller.java
+++ src/main/java/view/FXML_Goods_Controller.java
@@ -3,6 +3,8 @@ package view;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import controller.StoreController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +12,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
@@ -24,10 +28,20 @@ import java.util.ResourceBundle;
 
 public class FXML_Goods_Controller implements Initializable{
 
-    ArrayList<Goods> founded;
+    private ObservableList<Goods> Data = FXCollections.observableArrayList(DataInfo.getGoods());
+
 
     @FXML
-    private TableView<?> tv_supply;
+    private TableView<Goods> tv_supply;
+
+    @FXML
+    private TableColumn<Goods, Integer> N_column;
+
+    @FXML
+    private TableColumn<Goods, String> NameColumn;
+
+    @FXML
+    private TableColumn<Goods, Double> Price_column;
 
     @FXML
     private GridPane main_grid;
@@ -44,25 +58,19 @@ public class FXML_Goods_Controller implements Initializable{
     @FXML
     private JFXTextField fld_search;
 
+        @FXML
+    void OnActionB_search(ActionEvent event) throws Exception {
+
+        ArrayList<Goods> founded = StoreController.Search_in_category(DataInfo.getGoods(), fld_search.getText());
+        Fill_table(founded);
+    }
+
     @FXML
-    void OnEnterPressed(KeyEvent event) throws Exception {
+    void On_Enter_pressed(KeyEvent event) throws Exception {
         if(event.getCode()== KeyCode.ENTER)
         {
             OnActionB_search(new ActionEvent());
         }
-    }
-
-        @FXML
-    void OnActionB_search(ActionEvent event) throws Exception {
-
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLShop.fxml"));
-//            Parent root = loader.load();
-//            FXML_Shop_Controller control = loader.getController();
-//            ArrayList<Goods> goods = control.getGoods();
-//            founded =  StoreController.Search_in_category(goods,fld_search.getText());
-
-        //FillingListView(founded);
-
     }
 
     @FXML
@@ -78,10 +86,23 @@ public class FXML_Goods_Controller implements Initializable{
 
         Scene scene = new Scene(root);
         Stage stage = (Stage)main_grid.getScene().getWindow();
+        stage.setTitle("Magazine");
         stage.setScene(scene);
         stage.show();
     }
 
     public void initialize(URL location, ResourceBundle resources) {
+
+        Fill_table(DataInfo.getGoods());
+    }
+
+    public void Fill_table (ArrayList<Goods> goods)
+    {
+        ObservableList<Goods> Data = FXCollections.observableArrayList(goods);
+        N_column.setCellValueFactory(new PropertyValueFactory<Goods, Integer>("id"));
+        NameColumn.setCellValueFactory(new PropertyValueFactory<Goods, String>("name"));
+        Price_column.setCellValueFactory(new PropertyValueFactory<Goods, Double>("price"));
+        tv_supply.setItems(Data);
+        tv_supply.getSortOrder().addAll(N_column);
     }
 }
